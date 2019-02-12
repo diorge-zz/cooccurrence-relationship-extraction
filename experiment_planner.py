987e805139fd80a333390546736b2800c4fccf51
Diorge Brognara <diorge.bs@gmail.com>
2019-02-12 19:42:06 -0200

    Added the new components to the planner and made the output directory automatically named after the current date (up to seconds precision)

--- experiment_planner.py
+++ experiment_planner.py
@@ -1,29 +1,38 @@
+import datetime
+import os
 import experiment
 import preproc
 import ontext
 
 
 def main():
-    OUTPUT_DIR = '~/data/ontext_experiments/31jan19'
     CACHE_DIR = '~/data/ontext_experiments/cache'
     BASE_SVO = '~/data/mall/sample4m.svo'
     CATEGORY1 = '~/data/mall/instances/politicianus'
     CATEGORY2 = '~/data/mall/instances/religion'
+    OUTPUT_BASE_DIR = '~/data/ontext_experiments'
+    DATETIME_FORMAT = '%Y_%m_%d.%H_%M_%S'
 
-    exp = experiment.Experiment(OUTPUT_DIR, CACHE_DIR,
+    now = datetime.datetime.now().strftime(DATETIME_FORMAT)
+    output_dir = os.path.join(OUTPUT_BASE_DIR, now)
+
+    exp = experiment.Experiment(output_dir, CACHE_DIR,
             experiment.ReadCategory(CATEGORY1, 1),
             experiment.ReadCategory(CATEGORY2, 2),
             preproc.FilterSentencesByOccurrence(5),
             preproc.FilterInstanceInCategory(),
             experiment.SvoToMemory(),
             ontext.BuildCooccurrenceMatrix(),
-            ontext.NormalizeMatrix())
+            ontext.NormalizeMatrix(),
+            ontext.OntextKmeans(k=5),
+            ontext.InstanceRanker())
 
     exp.add_file('svo', BASE_SVO)
     exp.prepare()
     exp.execute_all()
 
-    print(exp.data['comatrix'])
+    print(exp.data['relation_names'])
+    print(exp.data['instances_scores'])
 
 
 if __name__ == '__main__':
