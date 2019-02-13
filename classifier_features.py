a0e6dcc53943b8ea3631b0670d64591f8717e99a
Diorge Brognara <diorge.bs@gmail.com>
2019-02-13 10:18:25 -0200

    Refactor instance frequency counter to better use the pandas features

--- classifier_features.py
+++ classifier_features.py
@@ -1,5 +1,4 @@
 import os
-import numpy as np
 import pandas as pd
 from collections import defaultdict
 
@@ -36,19 +35,26 @@ class InstanceFrequencyCount:
         return ['mean_instance_frequency_cat1', 'mean_instance_frequency_cat2']
 
     def apply(self, svo, cat1, cat2, output_dir, **kwargs):
-        frequencies1 = self.normalize(self.count(svo, cat1))
-        frequencies2 = self.normalize(self.count(svo, cat2))
+        frequencies1 = self.count(svo, cat1)
+        frequencies2 = self.count(svo, cat2)
         
         freq1_df = pd.DataFrame({'instance': list(frequencies1.keys()),
                                  'frequency': list(frequencies1.values())})
         freq2_df = pd.DataFrame({'instance': list(frequencies2.keys()),
                                  'frequency': list(frequencies2.values())})
 
+        # "normalizes" by settings the range 0-1 linearly,
+        # ie. divides by the maximum
+        freq1_df['normalized'] = freq1_df['frequency'] / freq1_df['frequency'].max()
+        freq2_df['normalized'] = freq2_df['frequency'] / freq2_df['frequency'].max()
+
+        # save intermediate step for later inspection
         freq1_df.to_csv(os.path.join(output_dir, 'instance_frequency_cat1'), index=False)
         freq2_df.to_csv(os.path.join(output_dir, 'instance_frequency_cat2'), index=False)
 
-        mean1 = np.fromiter(frequencies1.values(), np.float).mean()
-        mean2 = np.fromiter(frequencies2.values(), np.float).mean()
+        # actual feature
+        mean1 = freq1_df['normalized'].mean()
+        mean2 = freq2_df['normalized'].mean()
 
         return {'mean_instance_frequency_cat1': mean1,
                 'mean_instance_frequency_cat2': mean2}
@@ -66,10 +72,3 @@ class InstanceFrequencyCount:
                     counter[o] += int(n)
 
         return counter
-
-    def normalize(self, counter):
-        """Simply divides every count by the maximum count
-        """
-        max_count = max(counter.values())
-        return {instance: (count / max_count)
-                for (instance, count) in counter.items()}
