459810d4fd688d6fb8b97bf278781afb96103f5d
diorge <diorge.bs@gmail.com>
2019-03-14 15:19:48 -0300

    Error handling: ignore category pairs that fail, but still move to the next pair

--- run.py
+++ run.py
@@ -17,35 +17,39 @@ LOGGING_FORMAT='%(levelname)s %(asctime)s %(funcName)s\t%(message)s'
 def run(category_pairs, output_dir):
 
     for cat1, cat2 in category_pairs:
-        directory_name = '_'.join([cat1, cat2])
-        cat1_dir = os.path.join(CATEGORY_DIR, cat1)
-        cat2_dir = os.path.join(CATEGORY_DIR, cat2)
-        pair_output_dir = os.path.join(output_dir, directory_name)
+        try:
+            directory_name = '_'.join([cat1, cat2])
+            cat1_dir = os.path.join(CATEGORY_DIR, cat1)
+            cat2_dir = os.path.join(CATEGORY_DIR, cat2)
+            pair_output_dir = os.path.join(output_dir, directory_name)
 
-        exp = experiment.Experiment(pair_output_dir, CACHE_DIR,
-                steps=(
-                    preproc.FilterSentencesByOccurrence(5),
-                    preproc.MinimumContextOccurrence(3),
-                    preproc.MinimumPairOccurrence(5),
-                    experiment.ReadCategories(cat1_dir, cat2_dir),
-                    preproc.FilterInstanceInCategory(),
-                    experiment.SvoToMemory(),
-                    ontext.BuildCooccurrenceMatrix(),
-                    ontext.NormalizeMatrix(),
-                    ontext.OntextKmeans(k=5),
-                    ontext.InstanceRanker(),
-                    ontext.EvidenceForPromotion(promoted_instances=50),
-                    classifier.InstanceFrequencyCount(),
-                    classifier.Specifity(),
-                    classifier.PatternContextSize(),
-                    classifier.RelationshipCharacteristics(),
-                    classifier.FeatureAggregator(save_output=True)),
-                prefix='vpreptriples')
+            exp = experiment.Experiment(pair_output_dir, CACHE_DIR,
+                    steps=(
+                        preproc.FilterSentencesByOccurrence(5),
+                        preproc.MinimumContextOccurrence(3),
+                        preproc.MinimumPairOccurrence(5),
+                        experiment.ReadCategories(cat1_dir, cat2_dir),
+                        preproc.FilterInstanceInCategory(),
+                        experiment.SvoToMemory(),
+                        ontext.BuildCooccurrenceMatrix(),
+                        ontext.NormalizeMatrix(),
+                        ontext.OntextKmeans(k=5),
+                        ontext.InstanceRanker(),
+                        ontext.EvidenceForPromotion(promoted_instances=50),
+                        classifier.InstanceFrequencyCount(),
+                        classifier.Specifity(),
+                        classifier.PatternContextSize(),
+                        classifier.RelationshipCharacteristics(),
+                        classifier.FeatureAggregator(save_output=True)),
+                    prefix='vpreptriples')
 
-        exp.add_file('raw_svo', BASE_SVO)
-        exp.add_file('svo', BASE_SVO)
-        exp.prepare()
-        exp.execute_all()
+            exp.add_file('raw_svo', BASE_SVO)
+            exp.add_file('svo', BASE_SVO)
+            exp.prepare()
+            exp.execute_all()
+        except Exception as e:
+            print(f'Category pair {cat1}, {cat2} failed')
+            print(str(e))
 
 
 def main():
